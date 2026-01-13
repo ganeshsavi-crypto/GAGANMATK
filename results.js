0654ca71cd47b7e28a1f179ac3abf6dd158e0096
ganeshsavi-crypto <ganeshpatilemlpp@gmail.com>
2026-01-13 12:24:51 +0530

    Update results.js

--- results.js
+++ results.js
@@ -1,1 +1,35 @@
-
+// Matka Results Data File
+// Update this file to change results
+var matkaResults = {
+    "lastUpdated": "2026-01-12T10:30:00",
+    "SRIDEVI": {
+        "time": "10.00 AM",
+        "open": ["2", "7", "5", "0"],
+        "jodi": ["25", "70", "52", "07"]
+    },
+    "MILAN DAY": {
+        "time": "01.30 PM",
+        "open": [],
+        "jodi": []
+    },
+    "KALYAN": {
+        "time": "02.30 PM",
+        "open": [],
+        "jodi": []
+    },
+    "SRIDEVI NIGHT": {
+        "time": "06.30 PM",
+        "open": [],
+        "jodi": []
+    },
+    "MILAN NIGHT": {
+        "time": "08.00 PM",
+        "open": [],
+        "jodi": []
+    },
+    "MAIN BAZAR": {
+        "time": "08.30 PM",
+        "open": [],
+        "jodi": []
+    }
+};
