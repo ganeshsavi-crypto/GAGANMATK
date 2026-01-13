84e1bec61ade123a91283272b197c49f6811e2aa
ganeshsavi-crypto <ganeshpatilemlpp@gmail.com>
2026-01-13 13:11:49 +0530

    Update results.js

--- results.js
+++ results.js
@@ -1,35 +1,46 @@
 // Matka Results Data File
-// Update this file to change results
+// LAST UPDATED: 13/01/2026
+// To update results: Edit the numbers below and save this file
+
 var matkaResults = {
-    "lastUpdated": "2026-01-12T10:30:00",
+    "lastUpdated": "2026-01-13T10:15:00Z",
+    
     "SRIDEVI": {
         "time": "10.00 AM",
         "open": ["2", "7", "5", "0"],
         "jodi": ["25", "70", "52", "07"]
     },
+    
     "MILAN DAY": {
         "time": "01.30 PM",
-        "open": [],
-        "jodi": []
+        "open": ["3", "8", "1", "4"],
+        "jodi": ["38", "81", "14", "43"]
     },
+    
     "KALYAN": {
         "time": "02.30 PM",
-        "open": [],
-        "jodi": []
+        "open": ["9", "2", "6", "5"],
+        "jodi": ["92", "26", "65", "59"]
     },
+    
     "SRIDEVI NIGHT": {
         "time": "06.30 PM",
-        "open": [],
-        "jodi": []
+        "open": ["4", "1", "8", "3"],
+        "jodi": ["41", "18", "83", "34"]
     },
+    
     "MILAN NIGHT": {
         "time": "08.00 PM",
-        "open": [],
-        "jodi": []
+        "open": ["7", "0", "3", "6"],
+        "jodi": ["70", "03", "36", "67"]
     },
+    
     "MAIN BAZAR": {
         "time": "08.30 PM",
-        "open": [],
-        "jodi": []
+        "open": ["5", "9", "2", "1"],
+        "jodi": ["59", "92", "21", "15"]
     }
 };
+
+// IMPORTANT: After changing numbers above, save this file
+// The main page will automatically show updated results
